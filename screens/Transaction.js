a0caf6e470b1651c96b661e36ca38ac104be910b
Sayoon-K <84568721+Sayoon-K@users.noreply.github.com>
2022-01-15 13:50:03 +0530

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+export default class TransactionScreen extends React.Component {
+  render(){
+  return (
+    <View>
+      <Text>TransactionScreen</Text>
+      
+    </View>
+  );
+}
+}
